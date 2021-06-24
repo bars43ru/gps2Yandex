a048a64ccf71d1831b45bcb73d09ac15b713537b
Alexsander <bars-494516@yandex.ru>
2021-06-24 23:45:44 +0300

    structure prodject

---
+++ src/Gps2Yandex.References.Test/UnitTestScheduleLoader.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Gps2Yandex.References.Test
+{
+    public class UnitTestScheduleLoader
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
